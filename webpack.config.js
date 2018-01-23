6a03f5ff67d0f0ef4414dbfe4d0d1f304b71c87d
Gringe920 <e-dam@163.com>
2018-01-23 09:41:59 +0800

    add: add npm modules webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
-var webpack = require('webpack');
-var path = require('path');
-module.exports = {
+
+const path = require('path');
+
+const config = {
     entry: path.join(__dirname, 'view', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'view', 'bundle'),
         filename: '[name].bundle.js',
-        publicPath: path.join(__dirname, 'res')
+        publicPath: 'bundle'
     },
     module: {
-        loaders: [{
-            test: /\.scss$/,
-            exclude: /node_modules/, 
-            loader: ['style-loader','css-loader', 'sass-loader']
-        },
-        {
-            test:/\.js|jsx$/,
-            exclude:/node_modules/,
-            loader:'babel-loader'
-        }]
+        loaders: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/, 
+                loader: ['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                test:/\.js|jsx$/,
+                exclude:/node_modules/,
+                loader:'babel-loader'
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: {
+            index: 'view/index.html'
+        }
     }
-}
+};
+
+module.exports = config;
